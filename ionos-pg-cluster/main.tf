3b9f46a8df053c93cca31e6694f40c5df8eeb331
Salima Hassouni <salima.hassouni@capgemini.com>
2023-08-30 08:35:15 +0200

    add ionos-pg-cluster

---
+++ ionos-pg-cluster/main.tf
@@ -0,0 +1,27 @@
+resource "ionoscloud_pg_cluster" "pg_cluster" {
+  postgres_version     = var.postgres_version
+  instances            = var.instances_count
+  cores                = var.cores
+  ram                  = var.memory
+  storage_size         = var.volume_size
+  storage_type         = var.volume_disk_type
+  display_name         = var.name
+  location             = var.datacenter_location
+  synchronization_mode = var.synchronization_mode
+
+  connections {
+    datacenter_id = var.datacenter_id
+    lan_id        = var.lan_id
+    cidr          = local.cidr
+  }
+
+  credentials {
+    username = var.username
+    password = var.password
+  }
+
+  maintenance_window {
+    day_of_the_week = var.maintenance_day
+    time            = format("%02d:00:00", var.maintenance_hour)
+  }
+}
---
+++ ionos-pg-cluster/variables.tf
@@ -0,0 +1,95 @@
+variable "name" {
+  description = "The friendly name of your cluster."
+  type        = string
+}
+
+variable "postgres_version" {
+  description = "The PostgreSQL version of your cluster."
+  type        = string
+}
+
+variable "instances_count" {
+  description = "The total number of instances in the cluster (one master and n-1 standbys)"
+  type        = number
+}
+
+variable "cores" {
+  description = "The number of CPU cores per replica."
+  type        = number
+}
+
+variable "memory" {
+  description = "The amount of memory per instance in megabytes."
+  type        = number
+}
+
+variable "volume_size" {
+  description = "The amount of storage per instance in MB."
+  type        = number
+}
+
+variable "volume_disk_type" {
+  description = "The storage type used in your cluster. (Default: SSD)"
+  type        = string
+  default     = "SSD"
+}
+
+variable "datacenter_id" {
+  description = "The datacenter to connect your cluster to."
+  type        = string
+}
+
+variable "datacenter_location" {
+  description = "The physical location where the cluster will be created. This will be where all of your instances live. Property cannot be modified after datacenter creation (disallowed in update requests)"
+  type        = string
+}
+
+variable "k8s_cluster_id" {
+  description = ""
+  type        = string
+}
+
+variable "lan_id" {
+  description = "The LAN to connect your cluster to."
+  type        = string
+}
+
+variable "subnet_mask" {
+  description = "The subnet mask to use for allowing connections from the LAN. (Default: 16)"
+  type        = string
+  default     = 16
+}
+
+variable "ip_block" {
+  description = "The number to be set in the last ip block. (Default: 101)"
+  type        = number
+  default     = 101
+}
+
+variable "username" {
+  description = "The username for the initial postgres user."
+  type        = string
+}
+
+variable "password" {
+  description = "The password for the initial postgres user."
+  type        = string
+}
+
+variable "maintenance_day" {
+  description = "The day of the week for the 4 hour-long maintenance window. (Default: Sunday)"
+  type        = string
+  default     = "Sunday"
+}
+
+variable "maintenance_hour" {
+  description = "The time of the day when the 4 hour-long maintenance window may start. (Default: 3)"
+  type        = number
+  default     = 3
+}
+
+variable "synchronization_mode" {
+  description = "Represents different modes of replication. Can have one of the following values: ASYNCHRONOUS, SYNCHRONOUS, STRICTLY_SYNCHRONOUS. This attribute is immutable(disallowed in update requests). (Default: ASYNCHRONOUS)"
+  type        = string
+  default     = "ASYNCHRONOUS"
+}
