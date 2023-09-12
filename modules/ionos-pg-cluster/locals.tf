246122caa6c2f15b9295d17078892499cc1936a8
aimee-889 <aimee.weiss@capgemini.com>
2023-09-12 10:59:55 +0000

    change location of locals for cidr workaround

--- modules/ionos-pg-cluster/locals.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  nicIndex = module.cidr_workaround.nicIndex
-  prefix   = module.cidr_workaround.prefix
-  full_ip  = cidrhost(local.prefix, var.ip_block)
-  cidr     = format("%s/%s", local.full_ip, var.subnet_mask)
-}
