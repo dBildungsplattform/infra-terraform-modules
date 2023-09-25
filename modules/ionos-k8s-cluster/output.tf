aca5fcab5588e3906a90f5650b8594b2b9d2f10a
Salima Hassouni <salima.hassouni@capgemini.com>
2023-09-25 16:07:15 +0200

    Instead of taking the first element in the list, we take an arbitrary element.

--- modules/ionos-k8s-cluster/output.tf
+++ modules/ionos-k8s-cluster/output.tf
@@ -8,8 +8,8 @@ output "cluster_id" {
   value = ionoscloud_k8s_cluster.cluster.id
 }
 output "nodepool_zone1_id" {
-  value = ionoscloud_k8s_node_pool.nodepool_zone1[0].id
+  value = ionoscloud_k8s_node_pool.nodepool_zone1[*].id
 }
 output "nodepool_zone2_id" {
-  value = ionoscloud_k8s_node_pool.nodepool_zone2[0].id
+  value = ionoscloud_k8s_node_pool.nodepool_zone2[*].id
 }
