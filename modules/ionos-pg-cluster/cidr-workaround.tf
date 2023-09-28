a07377d262a13fd148f0d33777e07ce5ce9a8b0b
aimee-889 <aimee.weiss@capgemini.com>
2023-09-28 12:14:53 +0000

    fix path

--- modules/ionos-pg-cluster/cidr-workaround.tf
+++ modules/ionos-pg-cluster/cidr-workaround.tf
@@ -1,5 +1,5 @@
 module "cidr_workaround" {
-  source         = "../../ionos-cidr-workaround"
+  source         = "../../modules/ionos-cidr-workaround"
   k8s_cluster_id = var.k8s_cluster_id
   lan_id         = var.lan_id
 }
