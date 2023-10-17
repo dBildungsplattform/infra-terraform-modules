195c251a51339d7afbe96dd187fc76a1201eca2d
aimee-889 <aimee.weiss@capgemini.com>
2023-10-17 11:38:41 +0000

    delete telekom provider

--- modules/ionos-application-loadbalancer/versions.tf
+++ modules/ionos-application-loadbalancer/versions.tf
@@ -1,9 +1,5 @@
 terraform {
   required_providers {
-    opentelekomcloud = {
-      version = "1.24.6"
-      source  = "opentelekomcloud/opentelekomcloud"
-    }
     ionoscloud = {
       source  = "ionos-cloud/ionoscloud"
       version = "6.3.6"
