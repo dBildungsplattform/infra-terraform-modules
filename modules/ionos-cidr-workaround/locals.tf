8aa8a77f8c6c24dfff384b9f532a22256ccfc21d
aimee-889 <aimee.weiss@capgemini.com>
2023-11-06 12:44:00 +0000

    change how node_lan_ip is generated - more robust

--- modules/ionos-cidr-workaround/locals.tf
+++ modules/ionos-cidr-workaround/locals.tf
@@ -1,5 +1,5 @@
 locals {
   nicIndex = index(data.ionoscloud_servers.k8s_node.servers[0].nics.*.lan, tonumber(var.lan_id))
   prefix   = format("%s/24", data.ionoscloud_servers.k8s_node.servers[0].nics[local.nicIndex].ips[0])
-  node_lan_ips = flatten(data.ionoscloud_servers.k8s_nodes[*].servers[*].nics[local.nicIndex].ips[0])
+  node_lan_ips = flatten([for server_idx, server in data.ionoscloud_servers.k8s_nodes[*].servers[*] : [ for nic in flatten(server[*].nics[*]) :  nic.ips if nic.lan == tonumber(var.lan_id) ]])
 }
