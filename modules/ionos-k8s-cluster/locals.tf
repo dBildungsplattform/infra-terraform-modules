5e7cf74246e198f0e011afa99c5975e7cbf188db
marhode <maximilian.rhode@capgemini.com>
2023-11-09 14:32:53 +0100

    Simplified check for legacy and scaling deployment

--- modules/ionos-k8s-cluster/locals.tf
+++ modules/ionos-k8s-cluster/locals.tf
@@ -28,10 +28,13 @@ locals {
     cpu_family = null
   }])
 
+  #Check if the custom_nodepool list is empty, if so use the legacy object
+  nodepool_list_check = var.custom_nodepools == [] ? local.legacy_object : var.custom_nodepools
+
   #check if both legacy and scaling should be used, if so merge legacy object into the object list if needed (default = false)
   #if false: No need to do anything because it is either legacy or scaling
   #if true: check if first object is legacy, if not only scaling objects are in the list => merge legacy into it
-  legacy_check = var.enable_legacy_and_scaling == false ? var.custom_nodepools : (var.custom_nodepools[0].purpose != "legacy" ? tolist(concat(var.custom_nodepools, local.legacy_object)) : var.custom_nodepools)
+  legacy_check = var.enable_legacy_and_scaling == false ? local.nodepool_list_check : tolist(concat(var.custom_nodepools, local.legacy_object))
 
   #Loop through our nodepool list to detect empty values and fill them with legacy values
   #Only required for downward compatibility and legacy nodepools (If no downward compatibility is required just use var.custom_nodepools to loop over)
