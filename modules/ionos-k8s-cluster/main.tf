47cc49e2a36b318043349a0fc2f454191a8ccf5c
Maximilian Greve <maximilian.greve@capgemini.com>
2023-11-20 07:20:32 +0000

    added moved block

--- modules/ionos-k8s-cluster/main.tf
+++ modules/ionos-k8s-cluster/main.tf
@@ -80,7 +80,7 @@ resource "ionoscloud_k8s_node_pool" "nodepool_legacy" {
   #conditional create is just another count, if auto_scaling=true set count to nodepools_per_zone_count
   #for_each = { for pool in var.custom_nodepools : pool.site_name => pool if var.environment != "prod"}
   #count = each.value.nodepool_per_zone_count 
-  name              = each.value.availability_zone == "ZONE_1" ? "${local.cluster_name}-zone1-nodepool-000000000":"${local.cluster_name}-zone2-nodepool-0000000000000" #each.key
+  name              = each.value.availability_zone == "ZONE_1" ? "${local.cluster_name}-zone1-nodepool-0":"${local.cluster_name}-zone2-nodepool-0" #each.key
   k8s_version       = ionoscloud_k8s_cluster.cluster.k8s_version
   allow_replace     = each.value.allow_node_pool_replacement
   # the lans are created as a dynamic block - they help to dynamically construct repeatable nested blocks
@@ -120,6 +120,15 @@ resource "ionoscloud_k8s_node_pool" "nodepool_legacy" {
   public_ips     = local.public_ip_pools != null ? local.public_ip_pools[each.key] : []
 }
 
+moved {
+  from  = module.ionos_k8s_cluster.ionoscloud_k8s_node_pool.nodepool_zone1[0]
+  to    = module.ionos_k8s_cluster.ionoscloud_k8s_node_pool.nodepool_legacy["infra-dev-schulcloud-ops-5426-ZONE_1-legacy-0"]
+}
+
+moved {
+  from  = module.ionos_k8s_cluster.ionoscloud_k8s_node_pool.nodepool_zone2[0]
+  to    = module.ionos_k8s_cluster.ionoscloud_k8s_node_pool.nodepool_legacy["infra-dev-schulcloud-ops-5426-ZONE_2-legacy-0"]
+}
 
 resource "ionoscloud_ipblock" "ippools" {
   for_each = {for np in local.nodepool_per_zone_creator : "${local.cluster_name}-${np.availability_zone}-${np.purpose}-${np.nodepool_index}" => np }
