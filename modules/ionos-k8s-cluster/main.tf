123b0d58620a3eca35f99405630b333b3e4f02be
marhode <maximilian.rhode@capgemini.com>
2023-11-23 14:37:35 +0100

    Test for ip lists inconsistence

--- modules/ionos-k8s-cluster/main.tf
+++ modules/ionos-k8s-cluster/main.tf
@@ -52,7 +52,7 @@ resource "ionoscloud_k8s_node_pool" "nodepool_scaling" {
   cores_count    = each.value.core_count
   ram_size       = each.value.ram_size
   storage_size   = each.value.storage_size
-  public_ips     = each.value.create_public_ip_pools ? ionoscloud_ipblock.ippools[each.key].ips : each.value.public_ips
+  public_ips     = each.value.create_public_ip_pools == true ? ionoscloud_ipblock.ippools[each.key].ips : each.value.public_ips[0]
 
 
   auto_scaling {
