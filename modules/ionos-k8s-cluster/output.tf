a75716c541c187805aa20f02a81d8573faa2e336
Maximilian Greve <maximilian.greve@capgemini.com>
2023-11-30 09:35:33 +0000

    add ids to output

--- modules/ionos-k8s-cluster/output.tf
+++ modules/ionos-k8s-cluster/output.tf
@@ -7,3 +7,15 @@ output "cluster_k8s_version" {
 output "cluster_id" {
   value = ionoscloud_k8s_cluster.cluster.id
 }
+output "nodepool_zone1_id" {
+  value = values(ionoscloud_k8s_node_pool.nodepool_legacy)[*].id
+}
+# output "nodepool_zone2_id" {
+#   value = ionoscloud_k8s_node_pool.nodepool_zone2[*].id
+# }
+# output "nodepool_zone1_ips" {
+#   value = concat(ionoscloud_ipblock.ippools_zone1[*].ips)
+# }
+# output "nodepool_zone2_ips" {
+#   value = concat(ionoscloud_ipblock.ippools_zone2[*].ips)
+# }
