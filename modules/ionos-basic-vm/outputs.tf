b86f9e1195f90ce59235f5481d1fe842197f2029
maxi418 <144135803+maxi418@users.noreply.github.com>
2024-02-22 12:42:02 +0100

    Ops-5975 ionos basic vm module (#23)
    * moved vm module from infra-sc
    
    * changed module path
    
    * remove user info
    
    * cleanup
    
    * add outputs
    
    * terraform-docs: automated action
    
    * add missing output
    
    * terraform-docs: automated action
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ modules/ionos-basic-vm/outputs.tf
@@ -0,0 +1,11 @@
+output "basic_vm_server_id" {
+    value = local.basic_vm_server.id
+}
+
+output "basic_vm_server_public_ip" {
+    value = local.basic_vm_server.primary_ip
+}
+
+output "basic_vm_server_nicuuid" {
+  value = local.basic_vm_server.nic[*].id
+}
