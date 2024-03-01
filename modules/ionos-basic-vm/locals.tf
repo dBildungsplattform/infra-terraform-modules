bdf6dafb1935a3ce0c4e94464fec9af3365ef157
aimee-889 <aimee.weiss@capgemini.com>
2024-03-01 11:08:09 +0000

    fix missing ssh_key for ionos server resource

--- modules/ionos-basic-vm/locals.tf
+++ modules/ionos-basic-vm/locals.tf
@@ -1,4 +1,5 @@
 locals {
+  technicaluser_key_path = var.initial_public_key
   public_ip = var.create_ip_pool ? ionoscloud_ipblock.public_ip[0].ips[0] : var.public_ip
   dns_zone_name = var.dns_zone_name_override == "" ? module.conventions_coordinates.dns_zone_name : var.dns_zone_name_override
   basic_vm_server = var.permanent_vm ? ionoscloud_server.basic_vm_server[0] : ionoscloud_server.basic_vm_server_not_permanent[0]
@@ -6,7 +7,7 @@ locals {
   user_data_vars = {
     "initial_user" = var.initial_user
     "initial_uid" = var.initial_uid
-    "initial_public_key" = var.initial_public_key
+    "initial_public_key" = file(var.initial_public_key)
   }
   user_data = base64encode(templatefile("${path.module}/${var.user_data_script}", local.user_data_vars))
 }
