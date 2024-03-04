c405c904b3c6bf9e7a8ea5adf36d9bb72c3ee4bf
aimee-889 <aimee.weiss@capgemini.com>
2024-03-04 06:45:48 +0000

    add dns name to outputs of basic vm module

--- modules/ionos-basic-vm/outputs.tf
+++ modules/ionos-basic-vm/outputs.tf
@@ -2,6 +2,10 @@ output "basic_vm_server_id" {
     value = local.basic_vm_server.id
 }
 
+output "basic_vm_server_dns" {
+    value = trim(join("", opentelekomcloud_dns_recordset_v2.a_records.*.name), ".")
+}
+
 output "basic_vm_server_public_ip" {
     value = local.basic_vm_server.primary_ip
 }
