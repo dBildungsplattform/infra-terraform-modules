14ffa7c10865b2924bbdaedf2220777185f55c13
Salima Hassouni <salima.hassouni@capgemini.com>
2024-05-31 14:12:01 +0000

    DBP-817- delete ionos-basic-vm-mariadb to resolve the issue with no code to commit

--- modules/ionos-basic-vm/data.tf
+++
@@ -1,7 +0,0 @@
-data "opentelekomcloud_dns_zone_v2" "dns_zone" {
-  name = local.dns_zone_name
-}
-
-data "ionoscloud_datacenter" "datacenter" {
-  id = var.datacenter
-}
--- modules/ionos-basic-vm/user-data.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sed -i 's/HISTSIZE=1000/HISTSIZE=10000/g' /root/.bashrc
-sed -i 's/HISTFILESIZE=2000/HISTFILESIZE=20000/g' /root/.bashrc
