3aa5fff61d2655ffdb1da1abc311b1edcd6db3e9
Salima Hassouni <salima.hassouni@capgemini.com>
2024-06-01 16:31:03 +0000

    DBP-817- create ionos-basic-vm-mariadb module

---
+++ modules/ionos-basic-vm-mariadb/main.tf
@@ -0,0 +1,61 @@
+#Establish a private network for internal communication between the basic VM and the MariaDB cluster.
+resource "ionoscloud_lan" "private_lan" {
+  datacenter_id = var.datacenter_id
+  public        = false
+   name         = "${var.server_name}-private"
+}
+
+module "basic-vm" {
+  source                            = "../../modules/ionos-basic-vm"
+  coordinates                       = var.coordinates
+  fulldomainname                    = var.fulldomainname
+  datacenter                        = var.datacenter_id
+  datacenter_location               = var.datacenter_location
+  cpu_family                        = var.cpu_family
+  ip_pool_name                      = var.ip_pool_name
+  basic_vm_server_memory            = var.server_memory
+  basic_vm_server_cpu               = var.server_cpu
+  basic_vm_volume_size              = var.volume_size
+  basic_vm_second_volume_size       = var.second_volume_size
+  basic_vm_second_volume_disk_type  = var.second_volume_disk_type
+  domain_without_top_level          = var.domain_without_top_level
+  top_level_domain                  = var.top_level_domain
+  image                             = var.image
+  server_name                       = var.server_name
+  ports                             = var.ports
+  create_lan                        = true
+  lan_id                            = ionoscloud_lan.private_lan.id
+
+  # Ensure the private LAN is created before the basic VM
+  depends_on = [ionoscloud_lan.private_lan]
+
+  initial_user       = "technicaluser"
+  initial_uid        = "2215"
+  initial_public_key = "../../../provision/sshkeys/technicaluser.pub"
+}
+
+resource "ionoscloud_mariadb_cluster" "mariadb_cluster" {
+  mariadb_version     = var.mariadb_version
+  instances            = var.instances_count
+  cores                = var.cores
+  ram                  = var.memory
+  storage_size         = var.volume_size
+  
+  connections {
+    datacenter_id = var.datacenter_id
+    lan_id        = ionoscloud_lan.private_lan.id
+    cidr          =  local.database_ip_cidr
+  }
+  display_name         = var.display_name
+
+  maintenance_window {
+    day_of_the_week = var.maintenance_day
+    time            = format("%02d:00:00", var.maintenance_hour)
+  }
+  
+  credentials {
+    username = var.mariadb_username
+    password = var.mariadb_password
+  }
+ 
+}
---
+++ modules/ionos-basic-vm-mariadb/variables.tf
@@ -0,0 +1,135 @@
+variable "k8s_cluster_id" {
+  description = ""
+  type        = string
+}
+
+variable "datacenter_id" {
+  type = string
+}
+
+variable "coordinates" {
+  type = map(any)
+}
+
+variable "domain_without_top_level" {
+  type = string
+  description = "domain without top level"
+}
+
+
+variable "top_level_domain" {
+  type        = map(any)
+  description = "Top-level domain mapping for environments"
+}
+
+variable "ports" {
+  type = list
+  description = "ports to open in the IONOS firewall provided as objects in a list [{protocol = ???, name = ???, port = ???, source_ip = ???}], source_ip is optional."
+  default = []
+}
+
+variable "datacenter_location" {
+  type = string
+  description = "Geographic location of the datacenter"
+}
+
+variable "fulldomainname" {
+  type = string
+  description = "Domain under which the basic VM server will be available"
+}
+
+variable "ip_pool_name" {
+  type = string
+  description = "Name of the IP pool"
+}
+
+variable "image" {
+  type = string
+  description = "Image UUID for the VM"
+}
+
+variable "server_memory" {
+  type = number
+  description = "Memory allocated to the VM"
+}
+
+variable "server_cpu" {
+  type = number
+  description = "CPU cores allocated to the VM"
+}
+
+variable "cpu_family" {
+  type = string
+  description = "Type of CPU to use"
+}
+
+variable "volume_size" {
+  type = number
+  description = "Size of the primary volume in GB"
+}
+
+variable "second_volume_size" {
+  type = number
+  description = "Size of the second volume in GB"
+}
+
+variable "second_volume_disk_type" {
+  type = string
+  description = "Disk type of the second volume"
+}
+
+variable "server_name" {
+  type = string 
+  description = "name of the vm"
+}
+variable "mariadb_version" {
+  type = string
+  description = "Version of MariaDB to deploy"
+}
+
+variable "instances_count" {
+  type = number
+  description = "Number of instances in the MariaDB cluster"
+}
+
+variable "cores" {
+  type = number
+  description = "Number of CPU cores per MariaDB instance"
+}
+
+variable "memory" {
+  type = number
+  description = "Memory per MariaDB instance in GB"
+}
+
+variable "display_name" {
+  type = string
+  description = "Display name for the MariaDB cluster"
+}
+
+variable "maintenance_day" {
+  type = string
+  description = "Maintenance day for the MariaDB cluster"
+  default = "Sunday"
+}
+
+variable "maintenance_hour" {
+  type = number
+  description = "Hour at which maintenance may start"
+  default = 3
+}
+
+variable "mariadb_username" {
+  description = "The username for the initial mariadb user"
+  type = string
+}
+
+variable "mariadb_password" {
+  description = "The password for the initial mariadb user"
+  type = string
+}
+
+variable "lan_id" {
+  type = string
+  description = "LAN ID for connecting the VM and MariaDB cluster"
+}
