2b410c8b5460f036500750b5d6c7fa06c3bd0b18
Salima Hassouni <salima.hassouni@capgemini.com>
2024-06-03 13:45:04 +0000

    DBP-817 add lan_id variable

--- modules/ionos-cidr-workaround/variables.tf
+++ modules/ionos-cidr-workaround/variables.tf
@@ -2,3 +2,7 @@ variable "k8s_cluster_id" {
   type        = string
   description = "UUID of the Kubernetes cluster that the LAN is in"
 }
+variable "lan_id" {
+  type        = string
+  description = "ID of the LAN whose IP we want to get"
+}
