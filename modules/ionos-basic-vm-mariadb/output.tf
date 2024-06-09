904298256613c857bf1e7654869892951742f3d7
Salima Hassouni <salima.hassouni@capgemini.com>
2024-06-09 17:38:28 +0000

    DBP-817-adjust naming convention & return basic-vm dns and basic-vm ip

--- modules/ionos-basic-vm-mariadb/output.tf
+++ modules/ionos-basic-vm-mariadb/output.tf
@@ -1,6 +1,6 @@
 output "basic_vm_lan_ip" {
   description = "The IP address assigned to the basic VM NIC"
-  value       = ionoscloud_nic.basic_vm_nic.ips[0]
+  value       = ionoscloud_nic.privacyidea_vm_nic.ips[0]
 }
 
 output "database_ip_cidr" {
@@ -11,4 +11,14 @@ output "database_ip_cidr" {
 output "database_ip" {
   description = "IP address of the MariaDB cluster"
   value = local.database_ip
+}
+
+output "basic_vm_server_dns" {
+  description = "DNS record associated with the basic VM server"
+  value = module.basic-vm.basic_vm_server_dns
+}
+
+output "basic_vm_server_public_ip" {
+  description=  "the public IP address assigned to the basic VM server"
+  value = module.basic-vm.basic_vm_server_public_ip
 }
