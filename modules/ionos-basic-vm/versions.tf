c57247c9f10e5a2cafe6df724dcc20a11c01db0a
Salima Hassouni <salima.hassouni@capgemini.com>
2024-06-20 06:58:45 +0000

    DBP-817-upgarde ionos provider version

--- modules/ionos-basic-vm/versions.tf
+++ modules/ionos-basic-vm/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     ionoscloud = {
       source  = "ionos-cloud/ionoscloud"
-      version = "6.4.14"
+      version = "6.4.18"
     }
     opentelekomcloud = {
       source = "opentelekomcloud/opentelekomcloud"
