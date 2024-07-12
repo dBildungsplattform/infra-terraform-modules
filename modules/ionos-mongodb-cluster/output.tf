74af6206a0e55f97cdffbe8216366b380cc8b40d
YannickEvers <112076936+YannickEvers@users.noreply.github.com>
2024-07-12 13:19:02 +0200

    OPS-6673-mongodb-add-output (#31)
    Add UUID of the cluster to the outputs of the module (needed for managing the users in ansible).
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- modules/ionos-mongodb-cluster/output.tf
+++ modules/ionos-mongodb-cluster/output.tf
@@ -2,3 +2,7 @@ output "connection_string" {
   description = "The URL pointing to your cluster."
   value       = ionoscloud_mongo_cluster.mongo_cluster.connection_string
 }
+output "uuid" {
+  description = "The UUID of the cluster."
+  value       = ionoscloud_mongo_cluster.mongo_cluster.id
+}
