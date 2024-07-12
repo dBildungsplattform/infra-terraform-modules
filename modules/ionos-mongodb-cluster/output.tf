74062f071268a36cc19d5718f0af2a9d09bb3c11
Yannick Evers <yannick-tim.evers@capgemini.com>
2024-07-12 10:08:08 +0000

    Add UUID to outputs

--- modules/ionos-mongodb-cluster/output.tf
+++ modules/ionos-mongodb-cluster/output.tf
@@ -2,3 +2,7 @@ output "connection_string" {
   description = "The URL pointing to your cluster."
   value       = ionoscloud_mongo_cluster.mongo_cluster.connection_string
 }
+output "uuid" {
+  description = "The URL pointing to your cluster."
+  value       = ionoscloud_mongo_cluster.mongo_cluster.id
+}
