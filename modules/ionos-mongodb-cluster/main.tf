3ef5507bee3653edc8bf6c21268bb8967ff32b5d
Yannick Evers <yannick-tim.evers@capgemini.com>
2024-07-18 15:46:59 +0000

    terraform_data resource only shifts the problem, use null_data_source instead

--- modules/ionos-mongodb-cluster/main.tf
+++ modules/ionos-mongodb-cluster/main.tf
@@ -16,22 +16,13 @@ resource ionoscloud_mongo_cluster "mongo_cluster" {
     cidr_list     = local.cidrs
   }
 
-  template_id = terraform_data.template_id_workaround.output
+  template_id = data.null_data_source.template_workaround.outputs["mongo_template_id"]
   edition     = var.resource_template == null ? "enterprise" : null
 
   maintenance_window {
     day_of_the_week = var.maintenance_day
     time            = format("%02d:00:00", var.maintenance_hour)
   }
-}
-
-# Workaround
-# There's probably a bug in the IONOS Provider: The DataSource ionoscloud_mongo_template doesn't define id
-# as computed, leading to an inconsistent final plan (id is null instead of unknown in the initial plan)
-# This resource is in between and the output is unknown in the initial plan, solving the problem.
-resource "terraform_data" "template_id_workaround" {
-  input = var.resource_template == null ? null : data.ionoscloud_mongo_template.mongo_template[0].id
-  depends_on = [ data.ionoscloud_mongo_template.mongo_template ]
 }
 
 resource "ionoscloud_mongo_user" "initial_mongo_user" {
