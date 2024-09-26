6cacdb19c9a2d047bb20a2b36250c4fe83d91bda
Marco Schiborr <marco.schiborr@capgemini.com>
2024-09-26 15:02:08 +0000

    NLB Listener can only assign one Lan, we need multiple nlb, one for each cc

--- modules/ionos-nlb-for-servicecenter/main.tf
+++ modules/ionos-nlb-for-servicecenter/main.tf
@@ -5,10 +5,6 @@ module "conventions_coordinates" {
   top_level_domain         = var.top_level_domain
 }
 
-# resource "ionoscloud_private_crossconnect" "service_cc" {
-#   name  = "${var.datacenter_name}-service-lan-cc"
-# }
-
 resource "ionoscloud_lan" "crossconnect_lan" {
   count         = length(var.crossconnects)
   datacenter_id = var.datacenter_id
@@ -20,31 +16,25 @@ resource "ionoscloud_lan" "crossconnect_lan" {
   ]
 }
 
-# resource "ionoscloud_lan" "service_lan" {
-#   name          = "${var.datacenter_name}-service-lan"
-#   datacenter_id = var.datacenter_id
-#   public        = false
-#   pcc           = data.ionoscloud_private_crossconnect.crossconnect.id
-# }
-
 resource "ionoscloud_networkloadbalancer" "nlb" {
+    count                 = length(var.crossconnects)
     datacenter_id         = var.datacenter_id
-    name                  = "${module.conventions_coordinates.global_identifier}-nlb"
+    name                  = "${module.conventions_coordinates.global_identifier}-nlb-${count.index}"
     listener_lan          = ionoscloud_lan.crossconnect_lan[0].id
-    ips                   = var.listener_ips
+    ips                   = var.listener_ips[count.index]
     target_lan            = var.nlb_target_lan_id
     lb_private_ips        = [ "${local.node_ip_part}.225/24" ]
 
 }
 
-#TODO for each rule
 resource "ionoscloud_networkloadbalancer_forwardingrule" "forward_k8" {
+    count                       = length(var.crossconnects)
     datacenter_id               = var.datacenter_id
-    networkloadbalancer_id      = ionoscloud_networkloadbalancer.nlb.id
-    name                        = "forward-k8s"
+    networkloadbalancer_id      = ionoscloud_networkloadbalancer.nlb.id[count.index]
+    name                        = "forward-logs-and-metrics"
     algorithm                   = "ROUND_ROBIN"
     protocol                    = "TCP"
-    listener_ip                 = var.listener_ips[0]
+    listener_ip                 = var.listener_ips[count.index]
     listener_port               = var.nlb_listener_port
     dynamic "targets" {
       for_each = var.node_nlb_lan_ips
