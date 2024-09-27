098bc086eef7055f342325c5c8ca1cf7261d01d6
Marco Schiborr <marco.schiborr@capgemini.com>
2024-09-27 14:08:52 +0000

    Remove unused locals

--- modules/ionos-nlb-for-servicecenter/locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-    parts_of_node_ip = split(".", var.node_nlb_lan_ips[0])
-    # shorten ip from <num>.<num>.<num>.<num> to <num>.<num>.<num>
-    node_ip_part = join(".", slice(local.parts_of_node_ip, 0, length(local.parts_of_node_ip) - 1))
-}
