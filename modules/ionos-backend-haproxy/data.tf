b4e61a8d4bccd114270e76ed1c5da8218d5676a2
M-Schiborr <112689259+M-Schiborr@users.noreply.github.com>
2024-10-21 18:40:36 +0200

    OPS-7060-Cleanup-all-haproxy-resources (#36)

--- modules/ionos-backend-haproxy/data.tf
+++
@@ -1,8 +0,0 @@
-data "opentelekomcloud_dns_zone_v2" "dns_zone" {
-  name = module.conventions.dns_zone_name
-}
-
-data "ionoscloud_private_crossconnect" "crossconnect" {
-  count = length(var.crossconnects)
-  name  = var.crossconnects[count.index]
-}
