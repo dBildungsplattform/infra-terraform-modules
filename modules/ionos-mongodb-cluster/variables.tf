e5f9b760433ab9a7902ae16ec76aa4e83de40446
simoncolincap <simon.colin@capgemini.com>
2024-12-13 12:29:11 +0000

    OPS-7588 Add IONOS CIDR toggle to MongoDB module

--- modules/ionos-mongodb-cluster/variables.tf
+++ modules/ionos-mongodb-cluster/variables.tf
@@ -53,6 +53,11 @@ variable "k8s_cluster_id" {
   type        = string
   default     = null
 }
+variable "k8s_cluster_cidr_workaround" {
+  description = "Switch to enable/disable the Kubernetes CIDR workaround to prevent issues when the Kubernetes cluster doesn't exist yet and ID is unknown."
+  type        = bool
+  default     = var.k8s_cluster_id != null
+}
 variable "lan_id" {
   description = "The LAN to connect your cluster to."
   type        = string
