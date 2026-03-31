a2238f6038dcd0b178074178f7dff4c863f0160b
Martin Jürgens <martin.juergens@capgemini.com>
2026-03-31 10:50:33 +0200

    feat: DBP-1752 kafka creds

--- modules/ionos-kafka-cluster/output.tf
+++ modules/ionos-kafka-cluster/output.tf
@@ -1,3 +1,15 @@
 output "kafka_cluster_id" {
   value = ionoscloud_kafka_cluster.kafka_cluster.id
 }
+
+output "kafka_user_credentials_certificate_authority" {
+  value = data.ionoscloud_kafka_user_credentials.kafka_user_credentials_ds.certificate_authority
+}
+
+output "kafka_user_credentials_certificate" {
+  value = data.ionoscloud_kafka_user_credentials.kafka_user_credentials_ds.certificate
+}
+
+output "kafka_user_credentials_private_key" {
+  value = data.ionoscloud_kafka_user_credentials.kafka_user_credentials_ds.private_key
+}
