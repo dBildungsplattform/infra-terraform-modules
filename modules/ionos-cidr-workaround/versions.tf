4a3b337814f405ef6aed10ff6f27e9eae1b99854
Martin Jürgens <martin.juergens@capgemini.com>
2026-03-31 23:05:50 +0200

    feat: DBP-1752 kafka creds

--- modules/ionos-cidr-workaround/versions.tf
+++ modules/ionos-cidr-workaround/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     ionoscloud = {
       source  = "ionos-cloud/ionoscloud"
-      version = "~>6.4"
+      version = "~>6.7.22"
     }
   }
   required_version = ">= 0.13"
