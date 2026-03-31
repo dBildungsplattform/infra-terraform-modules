9efa07a088078a099520855c23c54af1be9304e4
Martin Jürgens <martin.juergens@capgemini.com>
2026-03-31 11:36:12 +0200

    feat: DBP-1752 kafka creds

--- modules/ionos-kafka-cluster/output.tf
+++ modules/ionos-kafka-cluster/output.tf
@@ -10,7 +10,7 @@ output "kafka_user_credentials_certificate" {
   value = data.ionoscloud_kafka_user_credentials.kafka_user_credentials_ds.certificate
 }
 
-# todo: directly write to secret store when terraform module is available
+# todo: directly write to secret store instead when terraform module is available
 output "kafka_user_credentials_private_key" {
   value = data.ionoscloud_kafka_user_credentials.kafka_user_credentials_ds.private_key
   sensitive = true
