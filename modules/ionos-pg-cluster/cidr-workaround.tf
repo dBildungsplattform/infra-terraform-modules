8c96683370ece1333462d5cddfa5e821c825791d
Martin Jürgens <martin.juergens@capgemini.com>
2026-04-01 08:36:28 +0200

    remove

--- modules/ionos-pg-cluster/cidr-workaround.tf
+++ modules/ionos-pg-cluster/cidr-workaround.tf
@@ -5,7 +5,6 @@ module "cidr_workaround" {
 }
 
 locals {
-  nicIndex = module.cidr_workaround.nicIndex
   prefix   = module.cidr_workaround.prefix
   full_ip  = cidrhost(local.prefix, var.ip_block)
   cidr     = format("%s/%s", local.full_ip, var.subnet_mask)
