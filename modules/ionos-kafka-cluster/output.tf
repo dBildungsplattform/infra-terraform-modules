52f685415f7fea3ab4ad8dc06ae70938b94df21e
Martin Jürgens <martin.juergens@capgemini.com>
2026-04-02 10:26:05 +0200

    output kafka broker addresses

--- modules/ionos-kafka-cluster/output.tf
+++ modules/ionos-kafka-cluster/output.tf
@@ -2,6 +2,10 @@ output "kafka_cluster_id" {
   value = ionoscloud_kafka_cluster.kafka_cluster.id
 }
 
+output "kafka_cluster_broker_addresses" {
+  value = ionoscloud_kafka_cluster.kafka_cluster.broker_addresses
+}
+
 output "kafka_cluster_certificate_authority" {
   value = data.ionoscloud_kafka_user_credentials.kafka_cluster_user_credentials.certificate_authority
 }
